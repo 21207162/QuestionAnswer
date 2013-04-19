64ac5ec56118475f5098137dc24f848dfdc651c1
Valentin BOUTONNÉ <valentin.boutonne@gmail.com>
2013-04-19 15:42:05 +0200

    display modifications

--- QuestionAnswer/grails-app/controllers/questionanswer/QuestionController.groovy
+++ QuestionAnswer/grails-app/controllers/questionanswer/QuestionController.groovy
@@ -27,7 +27,7 @@ class QuestionController {
         }
 
         flash.message = message(code: 'default.created.message', args: [message(code: 'question.label', default: 'Question'), questionInstance.id])
-        redirect(action: "show", id: questionInstance.id)
+        redirect(controller:"Answer", action: "create", id: questionInstance.id)
     }
 
     def show(Long id) {
